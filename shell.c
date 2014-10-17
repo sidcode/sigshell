fddec6ee01ccf41d3589d653efb229db1127422f
Gokula Krishnan <gokul.uf@gmail.com>
2014-10-18 00:31:17 +0530

    Removed header files and #defines
    Removed header files and #define s and put them in a separate file "headers.h"

--- shell.c
+++ shell.c
@@ -1,20 +1,4 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<signal.h>
-#include<sys/types.h>
-#include<string.h>
-#include<errno.h>
-
-
-#define CMD_LENGTH_MAX 50
-#define JOB_QUEUE_MAX 50
-
-//#defines for various process statuses
-#define RUN_FG 0
-#define RUN_BG  1
-#define STP_BG 2
-#define TERMINATED 3
+#include "headers.h"
 
 //Signal received Variable
 int sig_received = 0;
